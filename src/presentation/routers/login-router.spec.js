329c7855d4617340194d0fe1b424fa937ba45672
Diego Almeida <dyhalmeida@gmail.com>
2022-01-25 10:28:43 -0300

    refactor: remove duplicated code

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,7 @@
 class LoginRouter {
-  route(httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+  route (httpRequest) {
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
