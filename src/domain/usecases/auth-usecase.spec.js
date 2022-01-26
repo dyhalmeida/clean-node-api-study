d47a7d7cf36f799558838ef6336ec2ebaf423b3a
Diego Almeida <dyhalmeida@gmail.com>
2022-01-26 06:58:24 -0300

    test: Should throw if no passsword is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -2,8 +2,9 @@ const { MissingParamError } = require('../../utils/errors')
 
 const makeSut = () => {
   class AuthUseCase {
-    async auth ({ email }) {
+    async auth ({ email, password }) {
       if (!email) throw new MissingParamError('email')
+      if (!password) throw new MissingParamError('password')
     }
   }
 
@@ -17,7 +18,13 @@ const makeSut = () => {
 describe('Auth UseCase', () => {
   test('Should throw if no email is provided', async () => {
     const { sut } = makeSut()
-    const promisse = sut.auth({ email: '' })
+    const promisse = sut.auth({ email: '', password: 'any_password' })
     expect(promisse).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should throw if no passsword is provided', async () => {
+    const { sut } = makeSut()
+    const promisse = sut.auth({ email: 'any_email@mail.com', password: '' })
+    expect(promisse).rejects.toThrow(new MissingParamError('password'))
   })
 })
