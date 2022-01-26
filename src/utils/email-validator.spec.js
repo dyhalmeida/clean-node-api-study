aa9b18541601f2957d4dd28fd1bb0e2f07e63d8c
Diego Almeida <dyhalmeida@gmail.com>
2022-01-26 05:58:13 -0300

    Should return true if validator returns true and should return false if validator returns false

---
+++ src/utils/email-validator.spec.js
@@ -0,0 +1,22 @@
+const validator = require('validator')
+
+class EmailValidator {
+  isValid (email) {
+    return validator.isEmail(email)
+  }
+}
+
+describe('Email validator', () => {
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('any_email@mail.com')
+    expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidator()
+    validator.isEmailValid = false
+    const isEmailValid = sut.isValid('invalid_email@mail.com')
+    expect(isEmailValid).toBe(false)
+  })
+})
